1465f72dea82da524b8713b355937970154aa5c1
4mohamedalaa <mohamed.elbatanouny28@gmail.com>
2022-05-23 01:23:07 +0200

    update outputs and playbook

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,20 @@ output "private_key" {
        value = tls_private_key.key_form.private_key_pem
        sensitive = true
 }
+output "rds_add" {
+  value = aws_db_instance.rds.address
+
+}
+output "rds_port" {
+  value = aws_db_instance.rds.port
+
+}
+output "redis_add" {
+  value = aws_elasticache_cluster.elasticache.cache_nodes[0].address
+
+}
+
+output "redis_port" {
+  value= aws_elasticache_cluster.elasticache.port
+
+}
