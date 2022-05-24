5f2a86d5a82055faa65b491661a8f2db86292ba3
4mohamedalaa <mohamed.elbatanouny28@gmail.com>
2022-05-24 10:10:32 +0200

    changed output

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -2,7 +2,7 @@ output "bastion_public_ip" {
        value = aws_instance.bastion.public_ip
 }
 output "application_private_ip" {
-       value = aws_instance.bastion.private_ip
+       value = aws_instance.application.private_ip
 }
 output "private_key" {
        value = tls_private_key.key_form.private_key_pem
